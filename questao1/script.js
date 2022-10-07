d5cf4cbb250d847454124450c5e6eab62c182c30
Carlos Jeronimo <99830597+carlosbrito99@users.noreply.github.com>
2022-10-06 21:21:07 -0300

    Delete questao1 directory

--- questao1/script.js
+++
@@ -1,7 +0,0 @@
-let x = parseInt(prompt("Digite um valor. Para encerrar, digite 0"));
-
-while(x != 0) {
-    document.write(x * x);
-    document.write("<br>");
-    x = parseInt(prompt("Digite um valor. Para encerrar, digite 0"));
-}
