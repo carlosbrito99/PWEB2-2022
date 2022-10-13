ab8c3496e419e4c0d9d7a268a50f193d064807ae
Carlos Jeronimo <99830597+carlosbrito99@users.noreply.github.com>
2022-10-13 17:16:50 -0300

    Add files via upload

---
+++ PWEB2-2022/questao7/script7.js
@@ -0,0 +1,14 @@
+let base = 9;
+let altura = 3;
+
+let area;
+let perimetro;
+
+document.write("O valor da Base e --> ", base +"<br>");
+document.write("O valor da Altura e --> ", altura +"<br>"+"<br>");
+
+area = base * altura;
+document.write("A Area do Retangulo e: ", area +"<br>");
+
+perimetro = 2*base+2*altura;
+document.write("A Area do Perimetro e: ", perimetro);
