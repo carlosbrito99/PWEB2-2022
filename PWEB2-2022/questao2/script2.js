80b3dcbd472edf0f0e66fd6acf578b919b9fba4b
Carlos Jeronimo <99830597+carlosbrito99@users.noreply.github.com>
2022-10-13 17:09:17 -0300

    Add files via upload

---
+++ PWEB2-2022/questao2/script2.js
@@ -0,0 +1,9 @@
+let x = parseInt(prompt("Digite um valor. Para encerrar, digite 0"));
+let soma= 0;
+
+
+while(x!=0){
+    document.write(soma = soma + x);
+    document.write("<br>");
+    x = parseInt(prompt("Digite um valor. Para encerrar, digite 0"));
+}
